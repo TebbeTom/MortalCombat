dacceef6ba73bf863edd57dfb98c6ea4de891402
Youns Azzarouali <yazzarouali1@gmail.com>
2025-05-12 16:36:16 +0200

    SpielerAnimation

--- core/src/main/java/io/example/mortal/PlayerAnimations.java
+++ core/src/main/java/io/example/mortal/PlayerAnimations.java
@@ -5,15 +5,26 @@ import com.badlogic.gdx.graphics.g2d.Animation;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
 
 public class PlayerAnimations {
-	public Animation<TextureRegion> standardAnim;
-	public Animation<TextureRegion> punchAnim;
-	public Animation<TextureRegion> kickAnim;
-	public Animation<TextureRegion> duckAnim;
-	private TextureRegion[] animationFrames;
-	
-	public PlayerAnimations() {
-		animationFrames = new TextureRegion[1];
-		animationFrames[0] = new TextureRegion(new Texture("Martial Hero/Sprites/Idle.png"), 75, 70, 50, 55);
-		standardAnim = new Animation<TextureRegion>(1f, animationFrames);
-	}
+
+    public Animation<TextureRegion> idleAnim;
+    public Animation<TextureRegion> runAnim;
+    public Animation<TextureRegion> attackAnim;
+    public Animation<TextureRegion> hurtAnim;
+
+    public PlayerAnimations() {
+        idleAnim = loadAnimation("Spieler/Martial Hero/Idle/MartialHeroIdle", 8, 0.1f);
+        runAnim = loadAnimation("Spieler/Martial Hero/Run/MartialHeroRun", 8, 0.1f);
+        attackAnim = loadAnimation("Spieler/Martial Hero/Attack/MartialHeroAttack", 6, 0.08f);
+        hurtAnim = loadAnimation("Spieler/Martial Hero/Hurt/MartialHeroHurt", 4, 0.12f);
+    }
+
+    private Animation<TextureRegion> loadAnimation(String basePath, int frameCount, float frameDuration) {
+        TextureRegion[] frames = new TextureRegion[frameCount];
+        for (int i = 0; i < frameCount; i++) {
+            String filePath = String.format("%s%03d.png", basePath, i);
+            Texture texture = new Texture(filePath);
+            frames[i] = new TextureRegion(texture);
+        }
+        return new Animation<>(frameDuration, frames);
+    }
 }
